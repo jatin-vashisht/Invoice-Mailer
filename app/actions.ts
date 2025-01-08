98b16bcdeaeb4a2896343c2b85d1867e95275828
Jatin Vashisht <jatinvashisht11@gmail.com>
2025-01-09 00:05:14 +0530

    Update invoiceLink for download

--- app/actions.ts
+++ app/actions.ts
@@ -105,7 +105,7 @@ export async function createInvoice(prevState: any, formData: FormData) {
       invoiceLink:
         process.env.NODE_ENV !== "production"
           ? `http://localhost:3000/api/invoice/${data.id}`
-          : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
+          : `https://invoice-mailer-alpha.vercel.app/api/invoice/${data.id}`,
     },
   });
 
@@ -193,7 +193,7 @@ export async function editInvoice(prevState: any, formData: FormData) {
       invoiceLink:
         process.env.NODE_ENV !== "production"
           ? `http://localhost:3000/api/invoice/${data.id}`
-          : `https://invoice-marshal.vercel.app/api/invoice/${data.id}`,
+          : `https://invoice-mailer-alpha.vercel.app/api/invoice/${data.id}`,
     },
   });
 
